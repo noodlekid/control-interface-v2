cdf94d5b6fd20714f0bd421c78a8ba5ccfd1e0af
Mateusz Tyc <tycmat9@gmail.com>
2024-07-02 12:48:40 -0400

    chore: Add GNSSFix component and related files

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Message, Topic } from "roslib";
+import RosConnection from "../components/RosConnection";
+
+
+function DashHome() {
+}
+
+export default DashHome;
---
+++ src/app/types/stdMsgTypes.d.ts
@@ -0,0 +1,4 @@
+export interface Header {
+    seq: number;
+    frame_id: string;
+}
