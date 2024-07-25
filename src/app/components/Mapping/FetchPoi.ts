bb1e21097496e2ef00a75c692821810f6cfe8de3
Mateusz Tyc <tycmat9@gmail.com>
2024-07-25 00:29:21 -0400

    refactor: Update Dockerfile base image to node:18-alpine3.16

--- src/app/components/Mapping/FetchPoi.ts
+++ src/app/components/Mapping/FetchPoi.ts
@@ -1,7 +1,9 @@
+"use client"
+
 import { Poi, Coordinate } from "@prisma/client";
 import useSWR from "swr";
 
-export function useFetchSinglePoi(id: string) {
+export function useFetchSinglePoi(id: string){
   const key = `/api/poi/${id}`;
 
   return useSWR<{ poi: Poi; coordinate: Coordinate }>([key], async () => {
