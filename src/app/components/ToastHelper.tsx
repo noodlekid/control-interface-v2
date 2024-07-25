492a694a0df28eebf74a458c016c815a0de73ce5
Mateusz Tyc <tycmat9@gmail.com>
2024-07-25 14:53:18 -0400

    feat: Add ToastHelper component for displaying toast notifications

---
+++ src/app/components/ToastHelper.tsx
@@ -0,0 +1,11 @@
+import { ToastOptions, toast } from "react-toastify";
+
+export const DEFAULT_TOAST_OPTIONS: ToastOptions = {
+  position: "top-right",
+  autoClose: 5000,
+  draggable: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  pauseOnFocusLoss: false,
+};
+
